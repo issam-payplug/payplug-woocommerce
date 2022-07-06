0a42e4b1d9f42e85bdf5c744084f6cce29bdaa0f
ihamed-ext@payplug.com <ihamed-ext@payplug.com>
2022-07-06 18:59:04 +0100

    jjj

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -54,8 +54,7 @@ rm -f payplug-woocommerce.zip
 rm -f payplug-woocommerce.zip
 
 # Set the api-qa endpoint
-PAYPLUG_ENDPOINT='https://api-qa.payplug.com'
-REPLACE_API_STRING="s,https://api.payplug.com,$PAYPLUG_ENDPOINT,g"
+REPLACE_API_STRING="s,https://api.payplug.com,https://api-qa.payplug.com,g"
 find "vendor/payplug/payplug-php/lib/Payplug/Core/APIRoutes.php" -exec sed -i "$REPLACE_API_STRING" {} +
 
 pwd
