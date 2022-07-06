063efd6b17cac38be2bc2f8190e77e340f851e3d
ihamed-ext@payplug.com <ihamed-ext@payplug.com>
2022-07-06 18:32:23 +0100

    github actions 13

--- bin/build-plugin-zip.sh
+++ bin/build-plugin-zip.sh
@@ -59,7 +59,7 @@ rm -f payplug-woocommerce.zip
 # Set the api-qa endpoint
 PAYPLUG_ENDPOINT='https://api-qa.payplug.com'
 REPLACE_API_STRING="s,https://api.payplug.com,$PAYPLUG_ENDPOINT,g"
-find . "vendor/payplug/payplug-php/lib//Payplug/Core/APIRoutes.php" -exec sed -i "$REPLACE_API_STRING" {} +
+find . "payplug-woocommerce/vendor/payplug/payplug-php/lib//Payplug/Core/APIRoutes.php" -exec sed -i "$REPLACE_API_STRING" {} +
 
 # Generate the plugin zip file
 status "Creating archive..."
